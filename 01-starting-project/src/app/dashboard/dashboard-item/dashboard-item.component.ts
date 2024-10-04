6557ebb6b07d7e13e27ed53e8aab79cb87f1fbd4
Lucas Landivar <landivarlucas67@gmail.com>
2024-10-04 15:04:01 -0300

    Property Binding: Repetition

--- 01-starting-project/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ 01-starting-project/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -8,9 +8,9 @@ import { Component, input, Input } from '@angular/core';
   styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
-  @Input({ required: true }) image!: { src: string; alt: string };
-  @Input({ required: true }) title!: string;
+  // @Input({ required: true }) image!: { src: string; alt: string };
+  // @Input({ required: true }) title!: string;
 
-  // image = input.required<{ src: string; alt: string }>();
-  // title = input.required<string>();
+  image = input.required<{ src: string; alt: string }>();
+  title = input.required<string>();
 }
