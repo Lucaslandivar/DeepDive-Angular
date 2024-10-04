6aa05c0e55bb5ad173039abaada360508645e269
Lucas Landivar <landivarlucas67@gmail.com>
2024-10-04 15:13:20 -0300

    Adding Forms to Component

--- 01-starting-project/src/app/dashboard/tickets/tickets.component.ts
+++ 01-starting-project/src/app/dashboard/tickets/tickets.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NewTicketsComponent } from './new-tickets/new-tickets.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [NewTicketsComponent],
   templateUrl: './tickets.component.html',
-  styleUrl: './tickets.component.css'
+  styleUrl: './tickets.component.css',
 })
-export class TicketsComponent {
-
-}
+export class TicketsComponent {}
