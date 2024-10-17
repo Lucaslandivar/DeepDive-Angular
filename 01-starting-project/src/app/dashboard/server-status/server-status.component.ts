63b90755ec43e79c3291973ab573d039eb6a5d82
Lucas Landivar <landivarlucas67@gmail.com>
2024-10-17 19:35:32 -0300

    test

--- 01-starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ 01-starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -10,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
 
-  constructor() {}
+  constructor() {
+    console.log('hello');
+  }
 
   ngOnInit() {
     setInterval(() => {
