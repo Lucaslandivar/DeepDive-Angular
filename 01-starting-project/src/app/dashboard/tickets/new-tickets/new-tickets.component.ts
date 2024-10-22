a6266dad68942847a3f1cc75a39fc3e3a37576b2
Lucas Landivar <landivarlucas67@gmail.com>
2024-10-21 21:14:53 -0300

    Handling Form Submissions: Repetition

--- 01-starting-project/src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
+++ 01-starting-project/src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-tickets',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-tickets.component.html',
   styleUrl: './new-tickets.component.css',
 })
-export class NewTicketsComponent {}
+export class NewTicketsComponent {
+  onSubmit() {
+    console.log('Submited');
+  }
+}
